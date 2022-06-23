87f77d09f252eacb499817590f6c92d049135121
Jesus Cruz <jesus.cruz@campusvirtual.aunarvillavicencio.edu.co>
2022-06-23 09:54:03 -0500

    App profiles

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -38,10 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
-    'pages.apps.PagesConfig',
     'ckeditor',
-    
+    'core',
+    'messenger',
+    'pages.apps.PagesConfig',
+    'profiles',
 ]
 
 MIDDLEWARE = [
