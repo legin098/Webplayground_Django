ee4db3ad1c10ae2f42f507c7b73a575f2a0c1fe9
Jesus Cruz <jesus.cruz@campusvirtual.aunarvillavicencio.edu.co>
2022-07-01 20:27:19 -0500

    Finalizacion WebPlayground

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from pages.urls import pages_patterns
 from profiles.urls import profiles_patterns
+from messenger.urls import messenger_patterns
 from django.conf import settings
 
 urlpatterns = [
@@ -27,7 +28,9 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('registration.urls')),
     #Paths de profiles
-    path('profiles/', include(profiles_patterns))
+    path('profiles/', include(profiles_patterns)),
+    #Paths de messenger
+    path('messenger/', include(messenger_patterns)),
 ]
 
 if settings.DEBUG:
